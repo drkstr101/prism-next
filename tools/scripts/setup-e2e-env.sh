0910263354d8848d085d34d30cdec3729a216e7c
Aaron Miller <amiller@aps.org>
2023-10-10 16:08:25 +0000

    chore: add e2e workflow

---
+++ tools/scripts/setup-e2e-env.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Install Cypress dependencies.
+sudo apt-get update &&
+  sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+    libgtk2.0-0 \
+    libgtk-3-0 \
+    libnotify-dev \
+    libgconf-2-4 \
+    libnss3 \
+    libxss1 \
+    libasound2 \
+    libxtst6 \
+    xauth \
+    xvfb &&
+  sudo rm -rf /var/lib/apt/lists/*
