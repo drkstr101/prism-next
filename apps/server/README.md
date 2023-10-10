947c340de4bf828ad03f2fbbaf24d132f55e18d5
Aaron Miller <amiller@aps.org>
2023-10-10 17:46:58 +0000

    chore: rename server project

--- apps/server/README.md
+++ apps/server/README.md
@@ -1,4 +1,4 @@
-# pubsweet-server
+# server
 
 Launches a pubsweet compatible server using express. The server documentation can be found [here](https://dev.coko.foundation/?path=/docs/common-libraries-server--page).
 
--- apps/server/jest.config.ts
+++ apps/server/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'pubsweet-server',
+  displayName: 'server',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/pubsweet-server',
+  coverageDirectory: '../../coverage/apps/server',
 };
